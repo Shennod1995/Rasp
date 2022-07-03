f7d6e498766aa9cabcdcdab09c38bd29bf465533
Shennod <96147981+Shennod@users.noreply.github.com>
2022-07-03 22:57:06 +0300

    Init medium8-1-4

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CleanCode_ExampleTask1
+{
+    class Player
+    {
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+
+        public void Move()
+        {
+            //Do move
+        }
+
+        public void Attack()
+        {
+            //attack
+        }
+    }
+
+    class Weapon
+    {
+        public float WeaponCooldown { get; private set; }
+        public int WeaponDamage { get; private set; }
+
+        private bool IsReloading()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Movement
+    {
+        public float MovementDirectionX { get; private set; }
+        public float MovementDirectionY { get; private set; }
+        public float MovementSpeed { get; private set; }
+    }
+}
