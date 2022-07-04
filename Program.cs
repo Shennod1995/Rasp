6a5a4289ed2ea1d876dccc9500c6bc8d71a65bbd
Shennod <96147981+Shennod@users.noreply.github.com>
2022-07-04 19:06:29 +0300

    Init CleanCode_ExampleTask30

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿private bool _isEnable;
+
+private void On()
+{
+    if (_isEnable)
+        _effects.StartEnableAnimation();
+}
+
+private void Off()
+{
+    if (_isEnable == false)
+        _pool.Free(this);
+}
